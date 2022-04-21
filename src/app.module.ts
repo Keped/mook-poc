ba47fccce613c3f726d03cfdfed283140ab81207
ziv-elementor <71638280+ziv-elementor@users.noreply.github.com>
2022-04-21 21:02:45 +0300

    song model

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,12 @@ import { SongsService } from './songs/songs.service';
   imports: [
     SequelizeModule.forRoot({
       dialect: 'postgres',
+      host: "ec2-63-35-156-160.eu-west-1.compute.amazonaws.com",
       models: [Song],
-      // port: 58586,
-      // password: 'password',
-      // username: 'postgres',
+      port: 5432,
+      database: "d2emjjpahc6fjn",
+      password: 'c2804650ae5f4cdab211130ae40fcf6511588de61419976f606c69afc308cbd0',
+      username: 'dmlsfmwwtuakqb',
     }),
     SongsModule,
   ],

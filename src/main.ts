f27ffb88e85b2e6d20a3a383f094caf97a2396a9
ziv-elementor <71638280+ziv-elementor@users.noreply.github.com>
2022-05-07 22:31:58 +0300

    swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Session Server')
+    .setDescription('Session Recvorder API ')
+    .setVersion('1.0')
+    .addTag('mooki')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT);
 }
 bootstrap();

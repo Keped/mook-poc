03fef5d5e2f2ef04d446f6f02330fdf78113e3f4
ziv-elementor <71638280+ziv-elementor@users.noreply.github.com>
2022-05-10 22:33:28 +0300

    uploader key mysetry solved

--- mooki-front-master/src/api/Uploader.tsx
+++ mooki-front-master/src/api/Uploader.tsx
@@ -2,13 +2,14 @@ import * as AWS from "@aws-sdk/client-s3";
 import { PutObjectAclCommandInput } from "@aws-sdk/client-s3";
 const client = new AWS.S3({ region: "REGION" });
 const uploader = {
-  upload: async (b: Blob)=> {
+  upload: async (b: Blob, startTime = new Date(), participantId = "1")=> {
     // async/await.
-    const params = {Bucket: "mooki-poc-recordings", Key: "arn:aws:kms:us-east-1:883456886475:key/6fa04e74-f1cb-4531-8c26-647a4a08ec51", Body: b } as PutObjectAclCommandInput
+    const params = {Bucket: "mooki-poc-recordings", Key: `${startTime.getTime()}-${participantId}`, Body: b } as PutObjectAclCommandInput
     try {
       console.log(params)
       const uploadResult = await client.putObject(params);
       console.log(uploadResult)
+      return uploadResult;
       // process data.
     } catch (err) {
       console.error({err})
